d9b9cc382446abe9a7ef2bd3a7bad394ae2c7bfc
IbbyKun <118244914+IbbyKun@users.noreply.github.com>
2023-12-02 19:10:59 +0500

    Table Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import Navbar from "./Navbar/navbar"
 import Home from "./HomePage/home"
+import TablePage from "./TablePage/tablePage"
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Home/>
+      <TablePage/>
     </>
   )
 }
