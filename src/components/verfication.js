98dc9323dea1782f41abb374d6716a86d715d9ec
saim <138590167+saim1525@users.noreply.github.com>
2023-12-02 08:18:28 -0800

    after changes to conflict file

--- src/components/verfication.js
+++ src/components/verfication.js
@@ -4,22 +4,22 @@ export default function verfication() {
   return (
     <div>
       
-<body class="bg-gray-100 flex items-center justify-center h-screen">
+<body className="bg-gray-100 flex items-center justify-center h-screen">
 
-<div class="bg-white p-8 rounded shadow-md w-96">
+<div className="bg-white p-8 rounded shadow-md w-96">
 
-    <h1 class="text-2xl font-bold mb-6">Verification Code</h1>
+    <h1 className="text-2xl font-bold mb-6">Verification Code</h1>
 
-    <p class="text-gray-700 mb-4">A 6-digit verification code has been sent to your email.</p>
+    <p className="text-gray-700 mb-4">A 6-digit verification code has been sent to your email.</p>
 
     <form action="#" method="post">
 
-        <div class="mb-4">
-            <label for="verificationCode" class="block text-gray-700 text-sm font-bold mb-2">Verification Code</label>
-            <input type="text" id="verificationCode" name="verificationCode" class="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-500" maxlength="6" required></input>
+        <div className="mb-4">
+            <label for="verificationCode" className="block text-gray-700 text-sm font-bold mb-2">Verification Code</label>
+            <input type="text" id="verificationCode" name="verificationCode" className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-500" maxlength="6" required></input>
         </div>
 
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
             Verify
         </button>
 
