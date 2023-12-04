948db427d4d18e38a4525448bd1a714c7fe814c9
IbbyKun <118244914+IbbyKun@users.noreply.github.com>
2023-12-05 01:09:48 +0500

    HeroNavbar/Section1/ResourcesFile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 // src/App.js
 import React from 'react';
-import Navbar from "./Navbar/navbar"
+import Navbar from './Navbar/navbar';
 import SavedFiles from './components/savedFiles';
+import HeroNavbar from './Hero/HomeNavbar';
+import Section1 from './Hero/Section1'
+import Section2 from './Hero/Section2'
 
 export default function App() {
   return (
     <>
-    <Navbar />
-    <SavedFiles/>
-   
+      <HeroNavbar/>
+      <Section1 />
+      <Section2/>
     </>
-  )
+  );
 }
