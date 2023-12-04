d34d3ff42c5a2a15553be8f20354ee6a363a5510
hammadahmed81009 <76005065+hammadahmed81009@users.noreply.github.com>
2023-12-05 02:35:49 +0500

    SentimentCard Implemented

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Navbar from "./Navbar/navbar"
 import SavedFiles from './components/savedFiles';
 import HomePage from './HomePage/home'
 import Sidebar from './Sidebar/sidebar';
+import SentimentCard from './ProductDashboard/SentimentCard';
 
 export default function App() {
   return (
     <>
+    <SentimentCard sentimentValue={714} sentimentType="positive" />
+    <SentimentCard sentimentValue={200} sentimentType="negative" />
+    <SentimentCard sentimentValue={200} sentimentType="neutral" />
     </>
   )
 }
