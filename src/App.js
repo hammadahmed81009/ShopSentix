2968dc4a0d6dd37b945a028bd1b64f7befe49e0c
saim <138590167+saim1525@users.noreply.github.com>
2023-12-05 11:54:03 -0800

    Section6 And Footer updated

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import HeroNavbar from './Hero/HomeNavbar';
 import Section1 from './Hero/Section1'
 import Section2 from './Hero/Section2'
 import Section4 from './components/section4';
+import Section6 from './Hero/Section6';
+import Footer from './Hero/Footer';
 
 export default function App() {
   return (
     <>
+    <Footer/>
     
-    <Section4/>
     </>
   );
 }
