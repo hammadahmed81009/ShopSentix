766027722335ba479978ae304911a71f79dce0f8
hammadahmed81009 <76005065+hammadahmed81009@users.noreply.github.com>
2023-12-10 02:54:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
 import React from 'react';
-import Navbar from './Navbar/navbar';
-import SavedFiles from './components/savedFiles';
-import HomePage from './HomePage/home'
-import Sidebar from './Sidebar/sidebar';
-import HeroNavbar from './Hero/HomeNavbar';
-import Section1 from './Hero/Section1'
-import Section2 from './Hero/Section2'
-import Section3 from './Hero/Section3'
-import Section4 from './Hero/Section4'
-import Section5 from './Hero/Section5'
-import Section6 from './Hero/Section6'
-import Footer from './Hero/Footer'
-
 
 export default function App() {
   return (
