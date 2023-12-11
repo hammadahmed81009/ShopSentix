3a93e7c9b4639af8a5667ddc625b3f88b5f638ea
hammadahmed81009 <76005065+hammadahmed81009@users.noreply.github.com>
2023-12-11 09:54:57 +0500

    TablePage - Loading Animation & Load More Button Added

--- src/frontend/TablePage/ProductCard.js
+++ src/frontend/TablePage/ProductCard.js
@@ -37,7 +37,7 @@ const ProductCard = ({ ImageURL, Title, CurrentPrice, stars }) => {
         <div className="flex-shrink-0 flex items-center justify-center p-4">
           {renderStars()}
           <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600">
-            Go Forward
+            Analyze
           </button>
         </div>
       </div>
